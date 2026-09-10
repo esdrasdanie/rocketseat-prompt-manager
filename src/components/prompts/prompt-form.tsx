f44c1979daba30de0924785049c24197d3d4b72c
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-09-10 15:53:27 -0300

    Melhorando o feedback para o usuário

--- src/components/prompts/prompt-form.tsx
+++ src/components/prompts/prompt-form.tsx
@@ -12,7 +12,13 @@ import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 import { CopyButton } from '../button-actions';
 import { Button } from '../ui/button';
-import { Form, FormControl, FormField, FormItem } from '../ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../ui/form';
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
 
@@ -55,7 +61,7 @@ export const PromptForm = () => {
 
         <FormField
           control={form.control}
-          name="content"
+          name="title"
           render={({ field }) => (
             <FormItem>
               <FormControl>
@@ -67,13 +73,14 @@ export const PromptForm = () => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
 
         <FormField
           control={form.control}
-          name="title"
+          name="content"
           render={({ field }) => (
             <FormItem>
               <FormControl>
@@ -84,6 +91,7 @@ export const PromptForm = () => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
